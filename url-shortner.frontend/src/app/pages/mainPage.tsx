1a33bfc77534d4b4109b694142416da85f8ccadb
Luca Weinert | WSL Ubuntu <lucaweinert8@gmail.com>
2024-06-27 14:35:48 +0200

    feat: added ApiService class and adjust some project structure

--- url-shortner.frontend/src/app/pages/mainPage.tsx
+++ url-shortner.frontend/src/app/pages/mainPage.tsx
@@ -3,42 +3,43 @@
 import React, { useState, useEffect } from "react";
 import { Button, Card } from "@nextui-org/react";
 import { Input } from "@nextui-org/react";
-import { Console } from "console";
+import { ApiService } from "../services/ApiService";
 
 export default function MainPage() {
     const [originalUrl, SetOriginalUrl] = useState<string>("");
     const [shortenUrl, SetShortenUrl] = useState<string>("");
+    const apiService = new ApiService()
 
     async function GetShortenUrl() {
-        try {
-            var response = await fetch("http://localhost:5120/short?fullUrl=" + originalUrl, { method: "POST" })
-            const shortenUrl = await response.json()
-            console.log(shortenUrl)
-            SetShortenUrl(shortenUrl.fullShortenUrl)
-        } catch (error) {
-            console.error("Errro fetching data...", error);
-        }
+        const url = await apiService.GetShortenUrl(originalUrl);
+        console.log("url", url);
+        SetShortenUrl(url);
     }
 
     return (
         <>
-            <div className="flex">
-                <Input type="text" placeholder="https://www.example.com" onValueChange={SetOriginalUrl} />
-                <Button color="primary" onClick={GetShortenUrl}>
-                    Get shorten URL
-                </Button>
-            </div>
-            <div>
-                <p>original URL:</p>
-                {originalUrl}
-            </div>
-            <div>
-                <p>shorten URL:</p>
-                <Input
-                    isReadOnly
-                    type="text"
-                    value={shortenUrl}>
-                </Input>
+            <div className="grid grid-col-2 gap-1 justify-stretch">
+                <div className="col-start-1 col-end-1 grid grid-col-2">
+                    <Input type="text" placeholder="https://www.example.com" onValueChange={SetOriginalUrl} />
+                    <Button color="primary" onClick={GetShortenUrl}>
+                        Get shorten URL
+                    </Button>
+                </div>
+
+                <div className="col-start-2 col-end-2">
+                    <div>
+                        <p>original URL:</p>
+                        {originalUrl}
+                    </div>
+                    <div>
+                        <p>shorten URL:</p>
+                        <Input
+                            isReadOnly
+                            type="text"
+                            value={shortenUrl}>
+                        </Input>
+                    </div>
+                </div>
             </div>
         </>
     );
